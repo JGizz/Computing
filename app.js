2fc3dc1f037f73cfc40dbae3085778d365a5c222
JoshuaGill <joshuagill@students.freyberg.ac.nz>
2017-06-12 11:28:24 +1200

    Spacing

--- app.js
+++ app.js
@@ -14,23 +14,14 @@ class Bus {
 }
 
 var milson = new Bus ("Milson", milsonStops, '#f69238', milsonTimesMonFri, milsonTimesFri)
-
 var roslyn = new Bus ("Milson", roslynStops, '#f69238', roslynTimesMonFri, roslynTimesFri)
-
 var awapuni = new Bus ("Milson", awapuniStops, '#f69238', awapuniTimesMonFri, awapuniTimesFri)
-
 var rugby = new Bus ("Milson", rugbyStops, '#f69238', rugbyTimesMonFri, rugbyTimesFri)
-
 var rhodes = new Bus ("Milson", rhodesStops, '#f69238', rhodesTimesMonFri, rhodesTimesFri)
-
 var takaro = new Bus ("Milson", takaroStops, '#f69238', takaroTimesMonFri, takaroTimesFri)
-
 var highbury = new Bus ("Milson", highburyStops, '#f69238', highburyTimesMonFri, highburyTimesFri)
-
 var brightwater = new Bus ("Milson", brightwaterStops, '#f69238', brightwaterTimesMonFri, brightwaterTimesFri)
-
 var fernlea = new Bus ("Milson", fernleaStops, '#f69238', fernleaTimesMonFri, fernleaTimesFri)
-
 var heights = new Bus ("Milson", heightsStops, '#f69238', heightsTimesMonFri, heightsTimesFri)
 
 
